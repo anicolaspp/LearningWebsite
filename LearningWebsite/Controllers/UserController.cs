18b46e2c7a73c6841daa0a5190c1e5f74827c421
Nicolas Perez <nperez@mustard.ipcoop.com>
2016-03-14 09:57:57 -0700

    test and UI

--- LearningWebsite/Controllers/UserController.cs
+++ LearningWebsite/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearningWebsite.Models;
 
 namespace LearningWebsite.Controllers
 {
@@ -12,6 +13,18 @@ namespace LearningWebsite.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserViewModel userView)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Singup(UserViewModel userView)
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void true_should_be_true()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
